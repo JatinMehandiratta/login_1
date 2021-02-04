0ebce8979b559ac726dbf663dabb9fd96ec74ab7
jatin-excellence <jatin_mehandiratta@excellencetechnologies.in>
2021-02-04 12:36:31 +0530

    edit validations

--- index.php
+++ index.php
@@ -12,23 +12,26 @@ if (isset($_GET['updatesuccess'])) {
 if (isset($_GET['deletesuccess'])) {
     echo  '<div class="alert alert-danger">Record Deleted Successfully</div>';
 }
-$results_per_page = 5;  
-  
-    //find the total number of results stored in the database  
-    $query = "select *from user_table";  
-    $result = mysqli_query($conn, $query);  
-    $number_of_result = mysqli_num_rows($result);  
-  
-    //determine the total number of pages available  
-    $number_of_page = ceil ($number_of_result / $results_per_page);  
-  
-    //determine which page number visitor is currently on  
-    if (!isset ($_GET['page']) ) {  
-        $page = 1;  
-    } else {  
-        $page = $_GET['page'];  
-    }  
-    $page_first_result = ($page-1) * $results_per_page;  
+if (isset($_GET['emptyfields'])) {
+    echo '<div class="alert alert-danger">Please fill all the fields all fields are required<div>';
+}
+$results_per_page = 5;
+
+//find the total number of results stored in the database  
+$query = "select *from user_table";
+$result = mysqli_query($conn, $query);
+$number_of_result = mysqli_num_rows($result);
+
+//determine the total number of pages available  
+$number_of_page = ceil($number_of_result / $results_per_page);
+
+//determine which page number visitor is currently on  
+if (!isset($_GET['page'])) {
+    $page = 1;
+} else {
+    $page = $_GET['page'];
+}
+$page_first_result = ($page - 1) * $results_per_page;
 ?>
 <div class="container-fluid mb-4">
     <div class="header">
@@ -58,17 +61,17 @@ $results_per_page = 5;
                         or die(mysqli_error($conn));
 
                     while ($row = mysqli_fetch_assoc($query)) { ?>
-                             <tr id="row<?php echo $row['id'];?>">
-                            <td id="fname_val<?php echo $row['id'];?>"><?php echo $row['firstname'] ?></td>
-                            <td id="lname_val<?php echo $row['id'];?>"><?php echo $row['lastname'] ?></td>
-                            <td id="age_val<?php echo $row['id'];?>"><?php echo $row['age'] ?></td>
-                            <td id="email_val<?php echo $row['id'];?>"><?php echo $row['email'] ?></td>
-                            <td id="gender_val<?php echo $row['id'];?>"><?php echo $row['gender'] ?></td>
-                            <td id="occu_val<?php echo $row['id'];?>"><?php echo $row['occupation'] ?></td>
+                        <tr id="row<?php echo $row['id']; ?>">
+                            <td id="fname_val<?php echo $row['id']; ?>"><?php echo $row['firstname'] ?></td>
+                            <td id="lname_val<?php echo $row['id']; ?>"><?php echo $row['lastname'] ?></td>
+                            <td id="age_val<?php echo $row['id']; ?>"><?php echo $row['age'] ?></td>
+                            <td id="email_val<?php echo $row['id']; ?>"><?php echo $row['email'] ?></td>
+                            <td id="gender_val<?php echo $row['id']; ?>"><?php echo $row['gender'] ?></td>
+                            <td id="occu_val<?php echo $row['id']; ?>"><?php echo $row['occupation'] ?></td>
                             <td>
                                 <button class="edit_button  btn-primary btn-sm" name="edit_btn" id="edit_button<?php echo $row['id']; ?>" value="edit" onclick="edit_row('<?php echo $row['id']; ?>');">Edit</button>
                                 <button type='button' class="save_button  btn-primary btn-sm" style="display: none;" name="save_btn" id="save_button<?php echo $row['id']; ?>" value="save" onclick="save_row('<?php echo $row['id']; ?>');">Save</button>
-                                <button type='button' class="delete_button btn-danger btn-sm"   name="del_btn"id="delete_button<?php echo $row['id']; ?>" value="delete" onclick="delete_row('<?php echo $row['id']; ?>');">Delete</button>
+                                <button type='button' class="delete_button btn-danger btn-sm" name="del_btn" id="delete_button<?php echo $row['id']; ?>" value="delete" onclick="delete_row('<?php echo $row['id']; ?>');">Delete</button>
                             </td>
                         </tr>
                     <?php } ?>
@@ -76,19 +79,21 @@ $results_per_page = 5;
             </table>
         </div>
     </div>
-    <?php  for($page = 1; $page<= $number_of_page; $page++) {  
-        echo '<a href = "index.php?page=' . $page . '">' . $page . ' </a>';  
+    <?php for ($page = 1; $page <= $number_of_page; $page++) {
+        echo '<a href = "index.php?page=' . $page . '">' . $page . ' </a>';
     }  ?>
 </div>
 
 <script type="text/javascript">
-        $(function () {
-            $("#save_btn" +id).show();
-        });
-    </script>
+    $(function() {
+        $("#save_btn" + id).show();
+    });
+</script>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="modify_record.js"></script>
+
 </body>
 
 </html>
--- modify_record.js
+++ modify_record.js
@@ -6,16 +6,16 @@ function edit_row(id) {
     var gender = document.getElementById("gender_val" + id).innerHTML;
     var occupation = document.getElementById("occu_val" + id).innerHTML;
 
-    document.getElementById("fname_val" + id).innerHTML = "<input type='text' id='fname_text" + id + "' value='" + fname + "'required>";
-    document.getElementById("lname_val" + id).innerHTML = "<input type='text' id='lname_text" + id + "' value='" + lname + "'required>";
-    document.getElementById("age_val" + id).innerHTML = "<input type='number' id='age_text" + id + "' value='" + age + "'required>";
-    document.getElementById("email_val" + id).innerHTML = "<input type='email' id='email_text" + id + "' value='" + email + "'required>";
-    document.getElementById("gender_val" + id).innerHTML = "<input type='text' id='gender_text" + id + "' value='" + gender + "'required>";
-    document.getElementById("occu_val" + id).innerHTML = "<input type='text' id='occu_text" + id + "' value='" + occupation + "'required>";
-    
-    
+    document.getElementById("fname_val" + id).innerHTML = "<input type='text' id='fname_text" + id + "' value='" + fname + "'>";
+    document.getElementById("lname_val" + id).innerHTML = "<input type='text' id='lname_text" + id + "' value='" + lname + "'>";
+    document.getElementById("age_val" + id).innerHTML = "<input type='number' id='age_text" + id + "' value='" + age + "'>";
+    document.getElementById("email_val" + id).innerHTML = "<input type='email' id='email_text" + id + "' value='" + email + "'>";
+    document.getElementById("gender_val" + id).innerHTML = "<input type='text' id='gender_text" + id + "' value='" + gender + "'>";
+    document.getElementById("occu_val" + id).innerHTML = "<input type='text' id='occu_text" + id + "' value='" + occupation + "'>";
+
+
     document.getElementById("edit_button" + id).style.display = "none";
-   document.getElementById("save_button" + id).style.display = "block";
+    document.getElementById("save_button" + id).style.display = "block";
 }
 
 function save_row(id) {
@@ -25,40 +25,85 @@ function save_row(id) {
     var email = document.getElementById("email_text" + id).value;
     var gender = document.getElementById("gender_text" + id).value;
     var occupation = document.getElementById("occu_text" + id).value;
-     console.log("ajaxhdfvb");
-    $.ajax
-        ({
-            type: 'post',
-            url: 'modifyRecord.php',
-            data: {
-                edit_row: 'edit_row',
-                row_id: id,
-                fname_val: fname,
-                lname_val: lname,
-                age_val: age,
-                email_val: email,
-                gender_val: gender,
-                occu_val: occupation    
-            },
-            success: function (response) {
-                if (response == "success") {
-                    document.getElementById("fname_val" + id).innerHTML = fname;
-                    document.getElementById("lname_val" + id).innerHTML = lname;
-                    document.getElementById("age_val" + id).innerHTML = age;
-                    document.getElementById("email_val" + id).innerHTML = email;
-                    document.getElementById("gender_val" + id).innerHTML = gender;
-                    document.getElementById("occu_val" + id).innerHTML = occupation;
 
-                    document.getElementById("edit_button" + id).style.display = "block";
-                    document.getElementById("save_button" + id).style.display = "none";
-                    alert("This row has been edited successfully");
+
+    function validate() {
+        function validateemail()  
+        {  
+        var x= email;  
+        var atposition=x.indexOf("@");  
+        var dotposition=x.lastIndexOf(".");  
+        if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+          alert("Please enter a valid e-mail address");  
+          return false;
+          }return true;
+        }
+        if (fname == null || fname == "") {
+            alert("Firstname is mandatory");
+            return false;
+        } if (lname == null || lname == "") {
+            alert("Lastname is mandatory");
+            return false;
+        } if (age == NaN || age == "") {
+            alert("Age is mandatory");
+            return false;
+        } if (email == null || email == "") {
+            alert("Email is mandatory");
+            return false;
+        }
+         if (email == null || email ==""  ) {
+            alert("Email is mandatory");
+            return false;
+        }if(!validateemail()){
+            return false;
+        }
+        if (gender == null || gender == "") {
+            alert("Gender is mandatory");
+            return false;
+        } if (occupation == null || occupation == "") {
+            alert("Occupation is mandatory");
+            return false;
+        }return true;
+    } 
+    if (validate()==true){
+        
+    
+        $.ajax
+            ({
+                type: 'post',
+                url: 'modifyRecord.php',
+                data: {
+                    edit_row: 'edit_row',
+                    row_id: id,
+                    fname_val: fname,
+                    lname_val: lname,
+                    age_val: age,
+                    email_val: email,
+                    gender_val: gender,
+                    occu_val: occupation
+                },
+                success: function (response) {
+                    if (response == "success") {
+                        document.getElementById("fname_val" + id).innerHTML = fname;
+                        document.getElementById("lname_val" + id).innerHTML = lname;
+                        document.getElementById("age_val" + id).innerHTML = age;
+                        document.getElementById("email_val" + id).innerHTML = email;
+                        document.getElementById("gender_val" + id).innerHTML = gender;
+                        document.getElementById("occu_val" + id).innerHTML = occupation;
+
+                        document.getElementById("edit_button" + id).style.display = "block";
+                        document.getElementById("save_button" + id).style.display = "none";
+                        alert("This row has been edited successfully");
+                    }
                 }
-            }
-        });
-}
+            });
+        }
+    }
+
+
 
 function delete_row(id) {
-   
+
     $.ajax
         ({
             type: 'post',
